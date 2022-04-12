f35dae4d8f54aa2c2fbe75a7965bec9e8a73fed8
HamzehFarrokhi <af.af1346@gmail.com>
2022-04-12 23:13:31 +0430

    new changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-//import { useState, useEffect } from 'react'
 import HeaderDiv from "./components/HeaderDiv/HeaderDiv"
 import MainDiv from "./components/MainDiv/MainDiv"
+import Navigation from "./components/NavigationFooter/Navigation"
 
 const App = () => {
   return (
     <div>
       <HeaderDiv />
       <MainDiv />
+      <Navigation />
     </div>
   )
 }
---
+++ src/components/NavigationFooter/Navigation.js
@@ -0,0 +1,14 @@
+import './Navigation.css'
+
+const Navigation = () => {
+    return (
+        <div className="navigate">
+            <a href="./"><i className="fa-light fa-cog navIcon"></i></a>
+            <a href="./"><i className="fa-light fa-bookmark navIcon"></i></a>
+            <a href="https://armateam.ir/react/Flower"><i className="fa-light fa-shop navIcon"></i></a>
+            <a href="https://armateam.ir/react/Dashboard"><i className="fa-light fa-home navIcon"></i></a>
+        </div>
+    )
+}
+
+export default Navigation
